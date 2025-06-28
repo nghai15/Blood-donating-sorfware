a91c9e6d07f197a73ed59169a78f2fc450638369
ThaiSon <sonthainguyen814@gmail.com>
2025-06-28 14:51:36 +0700

    Update BloodUnitService.java

--- src/main/java/com/example/blooddonation/service/BloodUnitService.java
+++ src/main/java/com/example/blooddonation/service/BloodUnitService.java
@@ -5,7 +5,7 @@ import com.example.blooddonation.repository.BloodUnitRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List; // <-- Dòng này cần được thêm vào
+import java.util.List; 
 import java.util.Optional;
 
 @Service
